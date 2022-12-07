ca2f74ec80f1e0c792817577913d7c71ccb6f163
r-zar <115659495+r-zar@users.noreply.github.com>
2022-12-07 17:00:07 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
-package KOLOKWIUM_PROBNEV2;
-
 public class Main {
 
     public static void main(String[] args) {
         Game game = new Game();
-
-        game.addPlayer(new PlayerComp());
-
+        for (int i=0; i<10; ++i)
+        game.addPlayer(new PlayerComp("Stefan"));
+        game.addPlayer(new PlayerComp("Ziutek"));
+        game.printPlayers();
+        game.removePlayer("Zocha");
+        game.printPlayers();
+        
         game.play();
     }
 }
@@ -20,4 +22,3 @@ public class Main {
 // int a; deklarujemy zmienna a typu int
 // int to typ danych/ 32 bity
 // zakres wartosci na bajcie
-
