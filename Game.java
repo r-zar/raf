a401c1c38244e66a52b6ea2ac883c691c92a6bfa
r-zar <115659495+r-zar@users.noreply.github.com>
2022-12-14 16:55:58 +0100

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,3 +1,5 @@
+package com.mycompany.main;
+
 import java.util.Random;
 import java.util.List;
 import java.util.ArrayList;
@@ -7,6 +9,7 @@ public class Game {
     private Player player;
     private List<Player> players = new ArrayList<>();
     private Random dice = new Random();
+    private Statistics statistics = new Statistics();
 
     public void addPlayer(Player player){
         if (nameExists(player)){
@@ -27,6 +30,41 @@ addPlayer(player);
         return false;
 }
 
+    public void printPlayers() {
+       System.out.println("---------------------");
+       players.forEach(player ->
+               System.out.println(player.getName()));
+}
+
+    public void play() {
+
+        int number;
+        int guess;
+
+        boolean repeat = true;
+        do {
+            number = dice.nextInt(6) + 1;
+            System.out.println("Wylosowane: " + number);
+
+            for (Player player : players){
+                guess = dice.nextInt(6) + 1;
+                System.out.println(player.getName() + ": " + guess);
+
+                if (number == guess) {
+                    System.out.println("Brawo!");
+                    statistics.winner(player);
+                    repeat = false;
+                } else {
+                    System.out.println("Zle!");
+                }
+            }
+        } while (repeat);
+    }
+    
+    public void printStatistics() {
+        statistics.print();
+    }
+    
     public void removePlayer(String name) {
 
 //for (int i=0; i<players.size(); ++i){
@@ -46,38 +84,4 @@ addPlayer(player);
        players.removeIf((Player player) -> player.getName().equals(name));
 
 }
-
-
-
-    public void printPlayers() {
-       System.out.println("---------------------");
-       players.forEach(player ->
-               System.out.println(player.getName()));
-}
-
-    public void play() {
-
-        int number;
-        int guess;
-
-        boolean repeat = true;
-        do {
-            System.out.println("---------------------");
-
-            number = dice.nextInt(6) + 1;
-            System.out.println("Wylosowane: " + number);
-
-            for (Player player : players){
-            guess = dice.nextInt(6) + 1;
-            System.out.println(player.getName() + ": " + guess);
-
-            if (number == guess) {
-                System.out.println("Brawo!");
-                repeat = false;
-            } else {
-                System.out.println("Zle!");
-            }
-            }
-        } while (repeat);
-    }
 }
