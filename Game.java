f41431d33c69a8932e14ec5115502e27f96c6ee7
r-zar <115659495+r-zar@users.noreply.github.com>
2022-12-14 16:56:19 +0100

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,87 +1,29 @@
 package com.mycompany.main;
 
-import java.util.Random;
-import java.util.List;
-import java.util.ArrayList;
+public class Main {
 
-public class Game {
+    public static void main(String[] args) {
+        
+        Game game = new Game();
+        game.addPlayer(new PlayerComp("Stefan"));
+        game.addPlayer(new PlayerComp("Ziutek"));
 
-    private Player player;
-    private List<Player> players = new ArrayList<>();
-    private Random dice = new Random();
-    private Statistics statistics = new Statistics();
-
-    public void addPlayer(Player player){
-        if (nameExists(player)){
-            players.add(player);
-        }
-else{
-player.setName(player.getName() + dice.nextInt(10));
-addPlayer(player);
-}
+        
+        for(int i=0; i<10; ++i)
+            game.play();
+        
+        game.printStatistics();
     }
-
-    private void nameExists(String name) {
-        for (Player player : players){
-            if (player.getName().equals(name)) {
-                return true;
-            }
-        }
-        return false;
 }
 
-    public void printPlayers() {
-       System.out.println("---------------------");
-       players.forEach(player ->
-               System.out.println(player.getName()));
-}
 
-    public void play() {
 
-        int number;
-        int guess;
+//        game.printPlayers();
+//        game.removePlayer("Zocha");
+//        game.printPlayers();
 
-        boolean repeat = true;
-        do {
-            number = dice.nextInt(6) + 1;
-            System.out.println("Wylosowane: " + number);
 
-            for (Player player : players){
-                guess = dice.nextInt(6) + 1;
-                System.out.println(player.getName() + ": " + guess);
 
-                if (number == guess) {
-                    System.out.println("Brawo!");
-                    statistics.winner(player);
-                    repeat = false;
-                } else {
-                    System.out.println("Zle!");
-                }
-            }
-        } while (repeat);
-    }
-    
-    public void printStatistics() {
-        statistics.print();
-    }
-    
-    public void removePlayer(String name) {
-
-//for (int i=0; i<players.size(); ++i){
-//if (players.get(i).getName().equals(name)){
-//players.remove(i);
-//}
-//}
-
-//for (Iterator<Player> it = players.iterator();
-//it.hasNext();) {
-//if (it.next().getName().equals(name)) {
-//it.remove();
-//break;
-//}
-//} 
-
-       players.removeIf((Player player) -> player.getName().equals(name));
-
-}
-}
+// int a; deklarujemy zmienna a typu int
+// int to typ danych/ 32 bity
+// zakres wartosci na bajcie
